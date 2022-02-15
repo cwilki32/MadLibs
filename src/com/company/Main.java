6a89542df0dba47c0959f2d7958c7f44286d31dd
Collin Wilkins <cwilki32@Collins-MBP.attlocal.net>
2022-02-15 15:52:28 -0500

    Created 3 size 5 arrays and imported Random generator

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,32 @@
 package com.company;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// Mad Libs
+    //Create 3 size 5 arrays
+        //One array should have 5 single word strings, each of which is a noun.
+        //One array should have 5 single word strings, each of which is an adjective.
+        //One array should have 5 single word strings, each of which is a verb.
+    //The Java Util class Random has a nextInt() method that can be used to get a random number
+        // from the range of zero to the number passed in as a parameter.
+    //Write a small story that randomly gets words from each array to fill in blank
+        // points in the story based on the word type.
+    //Don’t forget to stage and commit changes in git as you go along
+    //Bonus: Get the words to fill each array by asking the user to supply them.
+
+        String [] listOfNouns = {"bird","dog","photograph","eyes","ocean"};
+        String [] listOfAdjectives = {"beautiful","blue","funny","giant","runny"};
+        String [] listOfVerbs = {"runs","jumps","flies","types","sleeps"};
+
+        Random rand = new Random();
+        int i;
+        i = rand.nextInt(5);
+
+        System.out.println("The " + listOfAdjectives[i] + " " + listOfNouns[i] + " " + listOfVerbs[i] + ".");
+
+
     }
 }
