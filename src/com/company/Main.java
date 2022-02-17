f6bd32ef93bfc9ba482d86b48d6fa29dcf5eba8e
Collin Wilkins <cwilki32@Collins-MBP.attlocal.net>
2022-02-17 15:55:40 -0500

    Mad Libs exercise

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,17 +6,17 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// Mad Libs
-    //Create 3 size 5 arrays
+        // Mad Libs
+        //Create 3 size 5 arrays
         //One array should have 5 single word strings, each of which is a noun.
         //One array should have 5 single word strings, each of which is an adjective.
         //One array should have 5 single word strings, each of which is a verb.
-    //The Java Util class Random has a nextInt() method that can be used to get a random number
+        //The Java Util class Random has a nextInt() method that can be used to get a random number
         // from the range of zero to the number passed in as a parameter.
-    //Write a small story that randomly gets words from each array to fill in blank
+        //Write a small story that randomly gets words from each array to fill in blank
         // points in the story based on the word type.
-    //Don’t forget to stage and commit changes in git as you go along
-    //Bonus: Get the words to fill each array by asking the user to supply them.
+        //Don’t forget to stage and commit changes in git as you go along
+        //Bonus: Get the words to fill each array by asking the user to supply them.
         //import Scanner
         //have user enter a noun, adjective, and verb
 
@@ -27,10 +27,11 @@ public class Main {
         String [] listOfVerbs = {"run","jump","fly","type","sleep"}; */
 
         //creating new lists with 5 inputs, user will generate
-        String [] listOfNouns = new String[5];
-        String [] listOfAdjectives = new String[5];
-        String [] listOfVerbs = new String[5];
+        String[] listOfNouns = new String[5];
+        String[] listOfAdjectives = new String[5];
+        String[] listOfVerbs = new String[5];
         //import new scanner
+
         Scanner userInput = new Scanner(System.in);
         Random rand = new Random();
         //have user build nouns
@@ -47,14 +48,13 @@ public class Main {
             listOfVerbs[i] = userInput.next();
         }
 
-
-
         //create madlibs story
         String story = "There once was a " + listOfAdjectives[rand.nextInt(5)] + " " +
-                listOfNouns[rand.nextInt(5)] + " that loved to " +listOfVerbs[rand.nextInt(5)] + ".";
+                listOfNouns[rand.nextInt(5)] + " that loved to " + listOfVerbs[rand.nextInt(5)] + "." +
+                '\n' + "Then a " + listOfAdjectives[rand.nextInt(5)] + " " + listOfNouns[rand.nextInt(5)]
+                + " made it " + listOfVerbs[rand.nextInt(5)] + ".";
 
         System.out.println(story);
-
 
     }
 }
